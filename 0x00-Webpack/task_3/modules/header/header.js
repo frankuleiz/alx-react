856e0ff5089c5aeb1aa66f90c81ae1140b197591
Frank <kamunyuz@gmail.com>
2024-05-21 05:53:51 +0300

    dev server

---
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import './header.css';
+
+console.log('Init header');
+$('body').append('<div id="logo"></div>');
+$('body').append('<h1>Holberton Dashboard</h1>');
---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: path.join(__dirname, 'public'),
+    port: 8564,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  mode: 'development',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: true,
+    }),
+  ],
+  devtool: 'inline-source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+};
