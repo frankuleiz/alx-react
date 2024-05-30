a767cb121bff95a1314bc5d624bbc1b74bc0a12d
Frank <kamunyuz@gmail.com>
2024-05-30 09:50:25 +0300

    chrome dev tools

--- 0x02-react_props/task_2/dashboard/src/Login/Login.js
+++ 0x02-react_props/task_2/dashboard/src/Login/Login.js
@@ -1,17 +1,17 @@
 import React from "react";
-import "./Login.css"
+import "./Login.css";
 
 function Login() {
-  return(
+  return (
     <React.Fragment>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
         <form>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
+          <input type="email" name="email" id="email" autoComplete="email"></input>
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
-          <button>OK</button>
+          <input type="password" name="password" id="password" autoComplete="current-password"></input>
+          <button type="submit">OK</button>
         </form>
       </div>
     </React.Fragment>
