e36aa1d308e377dd888f1497044cbbe91631cb47
Frank <kamunyuz@gmail.com>
2024-06-21 05:57:47 +0300

    read data from a json

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.test.js
+++
@@ -1,5 +0,0 @@
-import * as notificationData from "../../../../notifications.json";
-
-export const function getAllNotificationsByUser(userId) {
-  return notificationData.filter((Notification) => notificationData.author.id === userId.id).map((Notification) => Notification.context);
-}
